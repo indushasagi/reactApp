8131e3d39390c5ff2efbb3ee36f6b264a29ba758
indushasagi <indusha.sagi@gmail.com>
2019-08-05 13:29:45 +0530

    changes for count increment
    changes for count increment

--- src/index.js
+++ src/index.js
@@ -3,14 +3,44 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-function App() {
+let Incrementcount = Basic_comp =>
+  class extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { count: 0 };
+      this.Increcount = this.Increcount.bind(this);
+    }
+    Increcount() {
+      this.setState({ count: this.state.count + 1 });
+    }
+    render() {
+      return (
+        <div>
+          <Basic_comp {...this.state} increment={this.Increcount} />
+        </div>
+      );
+    }
+  };
+
+const Button = props => {
+  return (
+    <div>
+      <button onClick={props.increment}> Count : {props.count}</button>
+    </div>
+  );
+};
+
+let Compclick = Incrementcount(Button);
+
+const App = () => {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happens!</h2>
+      <h2>Start editing to see some magic happen!</h2>
+      <Compclick />
     </div>
   );
-}
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
